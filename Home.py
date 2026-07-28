4b31121b37f6483c085e21dc9c285507185e1f0b
ApollocreedXI <107815043+ApollocreedXI@users.noreply.github.com>
2026-07-28 22:58:00 +0000

    Finalized the vulnerability clustering

--- Home.py
+++ Home.py
@@ -5,29 +5,28 @@ st.set_page_config(layout="wide")
 
 st.sidebar.title("About")
 st.sidebar.info("""
-    - Web App URL: <https://streamlit.gishub.org>
-    - GitHub repository: <https://github.com/opengeos/streamlit-geospatial>
+    - Web App URL: <https://water-resilience-database-design.streamlit.app/>
+    - GitHub repository: <https://github.com/ApollocreedXI/streamlit-geospatial>
     """)
 
 st.sidebar.title("Contact")
 st.sidebar.info("""
-    Qiusheng Wu at [wetlands.io](https://wetlands.io) | [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://youtube.com/@giswqs) | [LinkedIn](https://www.linkedin.com/in/giswqs)
+    James Schorr [schorr@bc.edu](mailto:schorr@bc.edu) | Roydan Cruz [cruzroy@bc.edu](mailto:cruzroy@bc.edu)
     """)
 
-st.sidebar.title("Support")
-st.sidebar.info("""
-    If you want to reward my work, I'd love a cup of coffee from you. Thanks!
-    [buymeacoffee.com/giswqs](http://buymeacoffee.com/giswqs)
-    """)
 
 
 st.title("Database Design for Climate-Resilient Water Management Systems in Tanzania ")
 
 st.markdown("""
-    Edit this later
+    In March 2025, Tanzania released its new water policy that includes the goal of creating a national water grid, along with a focus on climate resilience in all planning and implementation.
+    This infrastructure investment intends to provide a reliable and stable water supply to citizens, including rural communities that are highly vulnerable to the impacts of climate change.
+    In developing this new infrastructure, policymakers will find it beneficial to evaluate communities that are most vulnerable to the impact of climate change (e.g., more susceptible to droughts and floods) and prioritize investment there.
+    Investment prioritization is a challenge as Tanzania’s water management system employs a decentralized structure, creating disparate information regarding administrative water basins and the Water User Associations (WUAs) subsumed in each of these administrative constructs.
+    The Capstone Project intends to unify information on the water basins and WUAs, to facilitate the identification of areas or communities under climate stress for investment prioritization. 
     """)
 
-st.info("Click on the left sidebar menu to navigate to the different apps.")
+st.info("Click on the left sidebar menu to navigate to the vulnerability clustering app and the exploritory dashboard.")
 
 st.subheader("Timelapse of Satellite Imagery")
 st.markdown("""
